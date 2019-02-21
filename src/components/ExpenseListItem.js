8dcbdb41e6491e56ef052edf514fa5656f96c5f0
isaacl <isaacl@isaacl.com>
2019-02-21 13:56:17 -0500

    seupt formatting for amount and createted at in list item comp

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import moment from 'moment';
+import numeral from 'numeral';
 
 import {Link} from 'react-router-dom'
 
@@ -10,8 +11,14 @@ export const ExpenseListItem = (props)=>{
         <div>
             <Link to={`/edit/${props.id}`}>
                 <h3>{props.description} </h3>
-            </Link> - {props.amount}
-           -
+            </Link>
+            <p>
+                {numeral(props.amount/100).format('$0,0.00')}
+                -
+                {moment(props.createdAt).format('MMMM Do, YYYY')}
+
+            </p>
+
 
         </div>
 

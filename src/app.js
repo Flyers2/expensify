66079316a643d05b208d87e407f6c9d9a5f5bc75
isaacl <isaacl@isaacl.com>
2019-02-21 11:09:43 -0500

    fixed typo AppRouter instead of Approuter in scr.sj

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
-import AppRouter from './router/Approuter'
+import AppRouter from './router/AppRouter'
 import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
